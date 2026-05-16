81c5e43ec349dcc6fb2643743f96e473bfa12d29
Bivek Gharti <gcbibek3353@gmail.com>
2026-05-16 09:54:42 +0530

    displaying the fallback image

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,1 +1,3 @@
 export const DICTIONARY_URL = 'https://api.dictionaryapi.dev/api/v2/entries/en/';
+import PLACEHOLDER_IMAGE from './placeholder.png';
+export const PLACEHOLDER_IMAGE_URL = PLACEHOLDER_IMAGE;
